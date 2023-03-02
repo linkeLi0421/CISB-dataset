e7d8290139ccf2968b4dc82e651336858e933427
linke <linkeli@smail.nju.edu.cn>
2023-03-02 01:16:14 +0000

    update reproduce set

--- reproduce_set/b_3-strlen-oob.c
+++ reproduce_set/b_3-strlen-oob.c
@@ -1,6 +1,6 @@
 /*successful
     x86-64 gcc 8-12.0.1 -O2
-    Compile and run the executable file, if the output is not "ok" and return 139, then there is a bug.
+    Compile and run the executable file, if the output dose not contain "done" and return 139, then there is a bug.
 */
 #include <stdio.h>
 #include <string.h>
@@ -16,23 +16,24 @@ int main(void)
         memset(src, 'A', sizeof(src));
         memset(dst, 'B', sizeof(dst));
 
-        //printf("MY_SIZE: %d\n", MY_SIZE);
+        printf("MY_SIZE: %d\n", MY_SIZE);
         fflush(NULL);
 
         size = MY_SIZE;
         ret = strlen(src);
         
         //if (size) {
-		size_t len = (ret >= size) ? size - 1 : ret;
+            size_t len = (ret >= size) ? size - 1 : ret;
 
-		memcpy(dst, src, len);
-		dst[len] = '\0';
+            memcpy(dst, src, len);
+            dst[len] = '\0';
         //}
 		if (strlen(dst))
-			printf("ok");
-        //printf("dst: %zu\n", strlen(dst));
+            puts("ok");
+
+        printf("dst: %zu\n", strlen(dst));
         fflush(NULL);
-        //printf("done...\n");
         fflush(NULL);
+        printf("done...\n");
         return 0;
 }
--- reproduce_set/b_9.c
+++ reproduce_set/b_9.c
@@ -1,5 +1,5 @@
 /*
-	x86-64 gcc4.1-12.0.1 -O1
+	x86-64 gcc4.1-7.4 -O1; gcc 8-12 -O0
 	x86-64 clang3.0.0-14.0.0 -O1
 	Compile and run the program, if output is "ok", it means assert(a+100 > a) is removed, so there is a bug.
 */
