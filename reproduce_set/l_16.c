cd1014d0ff7ad92e935b0a1b9554075a0cda588f
Zhengjie Du <dzj100@qq.com>
2023-03-02 17:15:02 +0800

    add main to l_16.c

--- reproduce_set/l_16.c
+++ reproduce_set/l_16.c
@@ -1,4 +1,5 @@
 // https://github.com/torvalds/linux/commit/02828845dda5ccf921ab2557c6ca17b6e7fc70e2
+#include <stdio.h>
 #define prefetch(ptr)				\
 	({					\
 		__asm__ __volatile__(		\
@@ -8,13 +9,21 @@
 		: "cc");			\
 	})
 
-extern void bar(char *x);
+void bar(char *x) {
+    printf("%c",*x);
+}
+
 void foo(char *x)
 {
 	prefetch(x);
 	if (x)                  //  x86-64  gcc 4.1.2 -O2
 		bar(x);
 }
+
+int main () {
+    foo(0);
+}
+
 
 /*
 asm code:
